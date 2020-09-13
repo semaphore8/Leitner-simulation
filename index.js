d9ccf11cadd18374df0bc4bf6a5024aecc42e079
sgbliznyuk <sgbliznyuk@gmail.com>
2020-09-13 12:42:16 +0300

    refactored classes

--- index.js
+++ index.js
@@ -1,64 +1,87 @@
 import settings from './settings.js'
 
 class Leitner {
-    constructor() {
-        this.newCardsCount = settings.cardsCount;
-        this.firstDeckCardsCount = 0;
-        this.secondDeckCardsCount = 0;
-        this.thirdDeckCardsCount = 0;
-        this.learnedCardsCount = 0;
+    constructor(settings, newGroup, firstGroup, secondGroup, thirdGroup, learnedGroup) {
+        this.newGroup = newGroup;
+        this.firstGroup = firstGroup;
+        this.secondGroup = secondGroup;
+        this.thirdGroup = thirdGroup;
+        this.learnedGroup = learnedGroup;
+        this.settings = settings;        
         this.currentDay = 0;
+        this.cardsFromEachGroup = Math.floor(settings.cardsADay / 4);
     }
 
-    renderCurrentDay() {
+    render() {
         let results = document.createElement('div');
-        wrapper.innerHTML = `<b>${this.currentDay} day:</b> ${this.newCardsCount} -> ${this.firstDeckCardsCount} -> ${this.secondDeckCardsCount} -> ${this.thirdDeckCardsCount} -> ${this.learnedCardsCount}`;
+        results.innerHTML = `<b>${this.currentDay} day:</b> ${this.newGroup.cardsCount} -> ${this.firstGroup.cardsCount} -> ${this.secondGroup.cardsCount} -> ${this.thirdGroup.cardsCount} -> ${this.learnedGroup.cardsCount}`;
         document.body.append(results);
     }
 
-    simulateCurrentDay() {
-        let newCardsCountResult;
-        let firstDeckCardsCountResult;
-        let secondDeckCardsCountResult;
-        let thirdDeckCardsCountResult;
-        let learnedCardsCountResult;
-
-        let cardsFromEachDeck = Math.round(settings.cardsADay / 4);
-        
-        let tdCardsToReview;
-        let sdCardsToReview;
-        let fdCardsToReview;
-        let newCardsToReview;
-
-        if (currentDay % settings.thirdDeckReviewInterval === 0) {
-            tdCardsToReview = this.thirdDeckCardsCount < cardsFromEachDeck ? this.thirdDeckCardsCount : cardsFromEachDeck;
-        } else {
-            tdCardsToReview = 0;
-        }
-
-        if (currentDay % settings.secondDeckReviewInterval === 0) {
-            sdCardsToReview = this.secondDeckCardsCount < cardsFromEachDeck ? this.secondDeckCardsCount : cardsFromEachDeck;
-        } else {
-            scCardsToReview = 0;
-        }  
-        if (currentDay % settings.firstDeckReviewInterval === 0) {
-            fdCardsToReview = this.firstDeckCardsCount < cardsFromEachDeck ? this.firstDeckCardsCount : cardsFromEachDeck;  
-        } else {
-            fdCardsToReview = 0;
-        }
-
-        newCardsToReview = settings.cardsADay - fdCardsToReview - sdCardsToReview - tdCardsToReview;
-        
-        newCardsCountResult = this.newCardsCount - Math.round(newCardsToReview * (1 - settings.errorsRate)) + 
-
-        function getSuccededCount(cardsCount) {
-            return Math.round(cardsCount * (1 - settings.errorsRate))
+    simulate() {
+        while (this.learnedGroup.cardsCount !== this.settings.cardsCount) {
+            console.log(this.learnedGroup.cardsCount, this.settings.cardsCount);
+            debugger;
+            this.simulateOneDay();
+            this.render();
         }
     }
 
-    carryCardsToReviewToday() {
-        if (this.thirdDeckCardsCount !== 0) 
+    simulateOneDay() {
+        // defining updates object - calculating passed/failed cards count:
+        let updates = {
+            [this.newGroup.name]: {},
+            [this.firstGroup.name]: {},
+            [this.secondGroup.name]: {},
+            [this.thirdGroup.name]: {},
+            [this.learnedGroup.name]: {},
+        };
+
+        function defineUpdates(group) {
+            if (this.currentDay % group.reviewInterval === 0) {
+                updates[group.name].count = group.cardsCount < this.cardsFromEachGroup ? group.cardsCount : this.cardsFromEachGroup;
+                updates[group.name].passed = Math.floor(updates[group.name].count * (1 - this.settings.errorsRate));
+                updates[group.name].failed = updates[group.name].count - updates[group.name].passed;
+            } else {
+                updates[group.name].count = 0;
+                updates[group.name].passed = 0;
+                updates[group.name].failed = 0;
+            }
+        }
+
+        defineUpdates.call(this, this.thirdGroup);
+        defineUpdates.call(this, this.secondGroup);
+        defineUpdates.call(this, this.firstGroup);
+        updates[this.newGroup.name].count = this.settings.cardsADay - updates[this.thirdGroup.name].count - updates[this.secondGroup.name].count - updates[this.firstGroup.name].count;
+
+        updates[this.newGroup.name].passed = Math.floor(updates[this.newGroup.name].count * (1 - this.settings.errorsRate));
+
+        updates[this.newGroup.name].failed = updates[this.newGroup.name].count - updates[this.newGroup.name].passed;
+
+        // applying updates:
+        //  new cards:
+        this.newGroup.cardsCount -= updates[this.newGroup.name].passed;
+        this.firstGroup.cardsCount += updates[this.newGroup.name].passed;
+        //  first group cards:
+        this.firstGroup.cardsCount -= updates[this.firstGroup.name].passed;
+        this.secondGroup.cardsCount += updates[this.firstGroup.name].passed;
+        this.firstGroup.cardsCount -= updates[this.firstGroup.name].failed;
+        this.newGroup.cardsCount += updates[this.firstGroup.name].failed;
+        //  second group cards:
+        this.secondGroup.cardsCount -= updates[this.secondGroup.name].passed;
+        this.thirdGroup.cardsCount += updates[this.secondGroup.name].passed;
+        this.secondGroup.cardsCount -= updates[this.secondGroup.name].failed;
+        this.firstGroup.cardsCount += updates[this.secondGroup.name].failed;
+        //  third group cards:
+        this.thirdGroup.cardsCount -= updates[this.thirdGroup.name].passed;
+        this.learnedGroup.cardsCount += updates[this.thirdGroup.name].passed;
+        this.thirdGroup.cardsCount -= updates[this.thirdGroup.name].failed;
+        this.secondGroup.cardsCount += updates[this.thirdGroup.name].failed;
+
+        this.currentDay++;
+
     }
+
 }
 
 class Group {
@@ -66,27 +89,13 @@ class Group {
         this.name = name;
         this.cardsCount = cardsCount;
     }
-
-    get name() {
-        return this.name;
-    }
-
-    get cardsCount() {
-        return this.cardsCount;
-    }
-
-    set cardsCount(value) {
-        this.cardsCount = value;
-    }
 }
 
 class ReviewableGroup extends Group {
-    constructor(name, cardsCount) {
+    constructor(name, cardsCount, reviewInterval) {
         super(name, cardsCount);
-
+        this.reviewInterval = reviewInterval;
     }
-
-    
 }
 
 function renderSettings() {
@@ -101,3 +110,13 @@ function renderSettings() {
 }
 
 renderSettings();
+
+let ng = new ReviewableGroup('new', settings.cardsCount, 1);
+let fg = new ReviewableGroup('first', 0, settings.firstDeckReviewInterval);
+let sg = new ReviewableGroup('second', 0, settings.secondDeckReviewInterval);
+let tg = new ReviewableGroup('third', 0, settings.thirdDeckReviewInterval);
+let lg = new Group('learned', 0);
+
+let leitner = new Leitner(settings, ng, fg, sg, tg, lg);
+
+leitner.simulate();
